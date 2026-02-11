84b3bfe255d0776025bc9a37210e7e7ce279c401
Tu Nombre <mmartinez@ucol.mx>
2026-02-11 08:42:52 -0600

    Agenda

---
+++ js/agenda.js
@@ -0,0 +1,28 @@
+document.write("Una agenda");
+
+let opcion = parseInt(prompt("Escoge una opción:  1-Agregar 2-listar 3-salir"));
+
+document.writeln(opcion);
+let seguir = true;
+let datos = [];
+while (seguir) { 
+    let opcion = parseInt(prompt("Escoge una opción:  1-Agregar 2-listar 3-salir"));
+    switch(opcion) {
+        case 1:
+            alert("Escogiste la opción 1");
+            let nombre = prompt("Ingresa tu nombre");
+            datos.push(nombre);
+            console.table(datos);
+            break;
+        case 2:
+            alert("Escogiste la opción 2");
+            break;
+        case 3:
+            alert("Escogiste la opción 3");
+            seguir = false;
+            break;
+        default:
+            alert("No recibí una opción válida");
+            break;
+    }
+}
